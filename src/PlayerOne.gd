7d45756bb9b92104586d12c2fe6924a15d805ff7
Luna <lunasmith@tutamail.com>
2019-12-01 17:11:20 -0300

    Tileset basic & Player One basic animation
    Tileset blue, red and green bricks added.
    Added walk and idle animation for player one.

--- src/PlayerOne.gd
+++ src/PlayerOne.gd
@@ -8,27 +8,30 @@ var direction = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$SpriteP1.animation = "idle"
+	$SpriteP1/AnimationPlayer.current_animation = "idle"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2()
 	#Gravedad
-	
+	var velocity = Vector2()
 	#Controles
 	if(Input.is_action_pressed("P1left")):
-		$SpriteP1.animation = "right"
+		$SpriteP1/AnimationPlayer.current_animation = "right"
 		$SpriteP1.flip_h = true
-		$SpriteP1.playing = true
 		velocity.x -= 50
 		
 	if(Input.is_action_pressed("P1right")):
-		$SpriteP1.animation = "right"
+		$SpriteP1/AnimationPlayer.current_animation = "right"
 		$SpriteP1.flip_h = false
-		$SpriteP1.playing = true
 		velocity.x += 50
 		
 	if(Input.action_press("P1jump")):
 		pass
+		
+	if(velocity.y == 0 && velocity.x == 0):
+		$SpriteP1/AnimationPlayer.current_animation = "idle"
 	
+	velocity.y += gravity * delta
 	position += velocity * delta
+	move_and_slide(velocity,Vector2(0,0))
+	
